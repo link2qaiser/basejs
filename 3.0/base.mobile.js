cfeaf332dc39f9c47926555b482dc21286eb1fdf
Qaiser S <link2qaiser@gmail.com>
2024-04-02 01:14:58 +0500

    fix mobile js

--- 3.0/base.mobile.js
+++ 3.0/base.mobile.js
@@ -1,9 +1,35 @@
 var baseMobileJS = {
     cdn: "https://dixeam.com/cdn",
+    site_url: "",
+    current_url: "",
+    langCDN: "https://cdn.statically.io/gh/link2qaiser/basejs/master/3.0/lang/en.js",
+    currentLocation: {},
+    /*
+    Some global methods
+    */
+    loadScript: function(url, callback) {
+        jQuery.ajax({
+            url: url,
+            dataType: "script",
+            success: callback,
+            async: true,
+        });
+    },
+    loadCSS: function(url, media, callback) {
+        jQuery(document.createElement("link"))
+            .attr({
+                href: url,
+                media: media || "screen",
+                type: "text/css",
+                rel: "stylesheet",
+            })
+            .appendTo("head")
+            .on("load", callback);
+    },
 
-    UserAuth : {
-        init: function(){
-            $(document).on("submit", '[data-action="do-login"]', function (e) {
+    UserAuth: {
+        init: function() {
+            $(document).on("submit", '[data-action="do-login"]', function(e) {
                 e.preventDefault();
                 var form = this;
                 $.ajax({
@@ -14,75 +40,105 @@ var baseMobileJS = {
                     dataType: "json",
                     url: $(form).attr("action"),
                     data: new FormData($(form)[0]),
-                    headers: { "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content") },
-                    success: function (res) {
-                        if(res.flag == true) {
+                    headers: {
+                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                    },
+                    success: function(res) {
+                        if (res.flag == true) {
                             baseMobileJS.Notification.showSucess(res.msg);
-                            localStorage.setItem("isLogin",true);
-                            localStorage.setItem("token",res.token);
-                            localStorage.setItem("user",JSON.stringify(res.user));
-                            window.location.href= $(form).attr("data-redirect");
-                            
-                        }else {
+                            localStorage.setItem("isLogin", true);
+                            localStorage.setItem("token", res.token);
+                            localStorage.setItem("user", JSON.stringify(res.user));
+                            window.location.href = $(form).attr("data-redirect");
+
+                        } else {
                             baseMobileJS.Notification.showError(res.msg);
                         }
                     },
                 });
             });
-            $(document).on("click", '[data-action="do-logout"]', function (e) {
+            $(document).on("click", '[data-action="do-logout"]', function(e) {
                 e.preventDefault();
                 localStorage.removeItem("isLogin");
                 localStorage.removeItem("token");
                 localStorage.removeItem("user");
-                window.location.href= $(this).attr("data-redirect");
+                window.location.href = $(this).attr("data-redirect");
 
             });
         }
     },
     Notification: {
         continer: ".notif-container",
-        init: function (param) {
+        init: function(param) {
             $("body").append('<div class="notif-container"><div class="notif"><div class="left"><i class="fas fa-check"></i></div><div class="right"><strong>Completed!</strong><div class="msg">Unable to login here</div></div><button>close</button></div></div>');
-            $(document).on("click", baseMobileJS.Notification.continer+' button', function (e) {
+            $(document).on("click", baseMobileJS.Notification.continer + ' button', function(e) {
                 $(baseMobileJS.Notification.continer).hide('fast');
             });
-            
+
         },
-        initMessage: function(){
+        initMessage: function() {
             $(baseMobileJS.Notification.continer + " .notif").removeClass("success info warning error");
             $(baseMobileJS.Notification.continer).show();
-            if($(baseMobileJS.Notification.continer).is(":visible") == true) {
-                setTimeout(function() { $(baseMobileJS.Notification.continer).fadeOut('fast'); }, 5000);
+            if ($(baseMobileJS.Notification.continer).is(":visible") == true) {
+                setTimeout(function() {
+                    $(baseMobileJS.Notification.continer).fadeOut('fast');
+                }, 5000);
             }
         },
-        showSucess: function (msg) {
+        showSucess: function(msg) {
             baseMobileJS.Notification.initMessage();
             $(baseMobileJS.Notification.continer + " .notif").addClass("success");
             $(baseMobileJS.Notification.continer + " strong").text("Completed!");
             $(baseMobileJS.Notification.continer + " .msg").html(msg);
         },
-        showInfo: function (msg) {
+        showInfo: function(msg) {
             baseMobileJS.Notification.initMessage();
             $(baseMobileJS.Notification.continer + " .notif").addClass("info");
             $(baseMobileJS.Notification.continer + " strong").text("Notice!");
             $(baseMobileJS.Notification.continer + " .msg").html(msg);
         },
-        showWarning: function (msg) {
+        showWarning: function(msg) {
             baseMobileJS.Notification.initMessage();
             $(baseMobileJS.Notification.continer + " .notif").addClass("warning");
             $(baseMobileJS.Notification.continer + " strong").text("Warning!");
             $(baseMobileJS.Notification.continer + " .msg").html(msg);
         },
-        showError: function (msg) {
+        showError: function(msg) {
             baseMobileJS.Notification.initMessage();
             $(baseMobileJS.Notification.continer + " .notif").addClass("error");
             $(baseMobileJS.Notification.continer + " strong").text("Oops!");
             $(baseMobileJS.Notification.continer + " .msg").html(msg);
         },
     },
-    init() {
-        baseMobileJS.UserAuth.init();
+    UserLocation: {
+        update(myCallback) {
+            const successCallback = (position) => {
+                myCallback(position.coords);
+            };
+            const errorCallback = (error) => {
+                myCallback(false);
+                //baseMobileJS.UserLocation.error = true;
+            };
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+        }
+    },
+    init(param) {
+        /*
+        Initailize the global variables
+        */
+        baseMobileJS.site_url = param.site_url;
+        baseMobileJS.current_url = param.current_url;
+        baseMobileJS.csrf_token = $('meta[name="csrf-token"]').attr("content");
+        /*
+        Default Initailize
+        */
         baseMobileJS.Notification.init();
+        baseMobileJS.UserAuth.init();
+
+        
+        baseMobileJS.loadScript(baseMobileJS.langCDN, function() {
+            baseMobileJS.lang = param.lang;
+        });
     }
+
 };
-baseMobileJS.init();
